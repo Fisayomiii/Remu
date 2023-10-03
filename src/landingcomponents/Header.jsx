267bd92fbf8749c6c497613ea9f0000e04404a55
Fisayomiii <wrld26993@gmail.com>
2023-10-03 16:16:33 +0100

    sign in and sign up page done

--- src/landingcomponents/Header.jsx
+++ src/landingcomponents/Header.jsx
@@ -12,31 +12,31 @@ const Header = () => {
     return (
         <>
 
-            <div class="fixed top-0 z-10 w-full mx-auto 2xl:max-w-7xl">
-                <div class="relative flex flex-col w-full p-5 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-36">
+            <div className="fixed top-0 z-10 w-full mx-auto 2xl:max-w-7xl">
+                <div className="relative flex flex-col w-full p-5 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-36">
 
-                    <div class="flex flex-row items-center justify-between lg:justify-start">
-                        <a class="flex items-center gap-1 text-lg tracking-tight text-white focus:outline-none focus:ring lg:text-2xl" href="/">
-                            <img src={logo} class="object-cover w-8 h-8 rounded-full" alt="" />
-                            <span class="lg:text-sm focus:ring-0">
+                    <div className="flex flex-row items-center justify-between lg:justify-start">
+                        <a className="flex items-center gap-1 text-lg tracking-tight text-white focus:outline-none focus:ring lg:text-2xl" href="/">
+                            <img src={logo} className="object-cover w-8 h-8 rounded-full" alt="" />
+                            <span className="lg:text-sm focus:ring-0">
                                 Remu
                             </span>
                         </a>
-                        <button class="inline-flex items-center justify-center p-2 text-gray-100 focus:outline-none focus:text-black md:hidden" onClick={toggleNavbar}>
-                            <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                                <path class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        <button className="inline-flex items-center justify-center p-2 text-gray-100 focus:outline-none focus:text-black md:hidden" onClick={toggleNavbar}>
+                            <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path className="inline-flex" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                                <path className="hidden" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
                     </div>
 
-                    <nav class="flex-col items-center flex-grow hidden md:pb-0 md:flex md:justify-end md:flex-row">
-                        <div class="inline-flex items-center gap-2 list-none lg:ml-auto">
-                            <div class="relative flex-shrink-0 ml-5">
-                                <a class="block px-9 py-4 mb-3 leading-loose text-[#fff] text-xs text-center font-semibold leading-none border-0 ring-2 ring-inset ring-[#fff] active:bg-[#fff] focus:outline-none focus:ring focus:ring-[#fff] transition duration-200 transition ease-in-out delay-150  duration-300 rounded-full" href="/" v-motion-pop-visible-once>Log in</a>
+                    <nav className="flex-col items-center flex-grow hidden md:pb-0 md:flex md:justify-end md:flex-row">
+                        <div className="inline-flex items-center gap-2 list-none lg:ml-auto">
+                            <div className="relative flex-shrink-0 ml-5">
+                                <a className="block px-9 py-4 mb-3 leading-loose text-[#fff] text-xs text-center font-semibold leading-none border-0 ring-2 ring-inset ring-[#fff] active:bg-[#fff] focus:outline-none focus:ring focus:ring-[#fff] transition duration-200 transition ease-in-out delay-150  duration-300 rounded-full" href="/" >Log in</a>
                             </div>
-                            <a class="block px-9 py-3 mb-2 leading-loose text-[#000] text-xs text-center font-semibold bg-[#fff] hover:bg-[#fff] active:bg-[#fff] focus:outline-none focus:ring focus:ring-[#fff] font-bold rounded-full transition duration-200 transition ease-in-out delay-150  duration-300"
-                                href="/" v-motion-pop-visible-once>Sign Up</a>
+                            <a className="block px-9 py-3 mb-2 leading-loose text-[#000] text-xs text-center font-semibold bg-[#fff] hover:bg-[#fff] active:bg-[#fff] focus:outline-none focus:ring focus:ring-[#fff] font-bold rounded-full transition duration-200 transition ease-in-out delay-150  duration-300"
+                                href="/" >Sign Up</a>
                         </div>
                     </nav>
 
@@ -56,10 +56,10 @@ const Header = () => {
                                     </button>
                                     <ul className="space-y-6 mt-10">
                                         <li >
-                                            <a class="block px-9 py-4 mb-3 leading-loose text-[#000] text-xs text-center font-semibold leading-none border-0 ring-2 ring-inset ring-[#000] active:bg-[#000] focus:outline-none focus:ring focus:ring-[#fff] transition duration-200 transition ease-in-out delay-150  duration-300 rounded-full" href="/" >Log in</a>
+                                            <a className="block px-9 py-4 mb-3 leading-loose text-[#000] text-xs text-center font-semibold leading-none border-0 ring-2 ring-inset ring-[#000] active:bg-[#000] focus:outline-none focus:ring focus:ring-[#fff] transition duration-200 transition ease-in-out delay-150  duration-300 rounded-full" href="/" >Log in</a>
                                         </li>
                                         <li>
-                                            <a class="block px-9 py-3 mb-2 leading-loose text-[#fff] text-xs text-center font-semibold bg-[#000] hover:bg-[#000] active:bg-[#000] focus:outline-none focus:ring focus:ring-[#fff] font-bold rounded-full transition duration-200 transition ease-in-out delay-150  duration-300" href="/">Sign Up</a>
+                                            <a className="block px-9 py-3 mb-2 leading-loose text-[#fff] text-xs text-center font-semibold bg-[#000] hover:bg-[#000] active:bg-[#000] focus:outline-none focus:ring focus:ring-[#fff] font-bold rounded-full transition duration-200 transition ease-in-out delay-150  duration-300" href="/">Sign Up</a>
                                         </li>
                                     </ul>
                                 </div>
---
+++ src/views/Register.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import demo from "../assets/demo.png";
+import remu from "../assets/remu.png";
+import "../styles/sign.css"
+import Signup from '../landingcomponents/Signup';
+import Login from '../landingcomponents/Login';
+
+
+function Register() {
+    const [loginMode, setLoginMode] = useState(true);
+
+    const toggleMode = () => {
+        setLoginMode(!loginMode);
+    };
+
+    return (
+        <>
+            <div className="full overflow-auto lg:overflow-hidden max-h-screen py-24 sm:py-1">
+                <div className="mx-auto max-w-1xl px-2 lg:px-0">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+
+                        <div className="relative z-10 flex flex-col flex-1 mt-[-30px] px-4 py-10 lg:py-2 md:flex-none md:px-0 sm:justify-center">
+                            <div className="w-full max-w-md mx-auto mr-42 md:max-w-sm md:px-0 md:w-96 sm:px-4">
+                                <div className="flex flex-col">
+                                    <div>
+                                        <img src={remu} className="mb-5 h-10 w-10 rounded-full" alt="logo" />
+                                        <h2 className="text-3xl text-white">{loginMode ? "Let's get started!" : "Welcome Back!"}</h2>
+                                        <p className="mt-2 text-sm text-gray-300">
+                                            Already have an account?
+                                            <span className='text-gray-400 underline cursor-pointer' onClick={toggleMode}>
+                                                {loginMode ? " Log in" : " Sign up"}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </div>
+                                {loginMode ? <Signup /> : <Login />}
+                            </div>
+                        </div>
+
+                        <div className="relative top-60 left-[10%] bg-[#212121] ">
+                            <img src={demo} alt="Product screenshot" className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 hidden lg:block sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Register
